30dd973377c4f30c71ef8b3b4ea0c63ed191c394
ArtisanGray <kitwithshorts@gmail.com>
2022-05-27 01:26:41 -0500

    recreated the project, initial review commit

--- 0x09-unity_ar_business_card/Assets/Scripts/ButtonClick.cs
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonClick.cs
@@ -1,30 +1,26 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
 public class ButtonClick : MonoBehaviour
 {
-    public GameObject button;
-    public VirtualButtonBehaviour vb;
-    public Animator startAni;
+    //public GameObject button;
+    //public VirtualButtonBehaviour vb;
+    //public Animator startAni;
     public string Url;
     // Start is called before the first frame update
     void Start()
     {
-        vb.RegisterOnButtonPressed(OnButtonPressed);
-        vb.RegisterOnButtonReleased(OnButtonReleased);
+        //vb.RegisterOnButtonPressed(OnButtonPressed);
+        //vb.RegisterOnButtonReleased(OnButtonReleased);
         //startAni.GetComponent<Animator>();
         //startAni.Play("start");
     }
-
-    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    public void GetURL()
     {
         Application.OpenURL(Url);
-        Debug.Log("Button Pressed!");
-    }
-    public void OnButtonReleased(VirtualButtonBehaviour vb)
-    {
+        Debug.Log(Url + " Opened");
     }
     // Update is called once per frame
     void Update()
